f703a86f22f234cc21e5fbae5f7538f9eb63bbfc
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2026-04-25 21:06:37 +0700

    pembenahan produk

--- src/pages/produk/index.tsx
+++ src/pages/produk/index.tsx
@@ -268,52 +268,52 @@
 
 // export default kategori;
 
-// import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
-// import TampilanProduk from "../../views/produk";
-// import useSWR from "swr";
-// import fetcher from "../../utils/swr/fetcher";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import TampilanProduk from "../../views/produk";
+import useSWR from "swr";
+import fetcher from "../../utils/swr/fetcher";
 
-// // const fetcher = (url: string) => fetch(url).then((res) => res.json());
+// const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-// const kategori = () => {
-//   // const [isLogin, setIsLogin] = useState(true);
-//   const { push } = useRouter();
-//   const [products, setProducts] = useState([]);
-//   // console.log("products:", products);
+const kategori = () => {
+  // const [isLogin, setIsLogin] = useState(true);
+  const { push } = useRouter();
+  const [products, setProducts] = useState([]);
+  // console.log("products:", products);
 
-//   const { data, error, isLoading } = useSWR("/api/produk", fetcher);
-//   //cek apakah data, error, dan isLoading sudah benar
+  const { data, error, isLoading } = useSWR("/api/produk", fetcher);
+  //cek apakah data, error, dan isLoading sudah benar
 
-//   return (
-//     <div>
-//       <TampilanProduk products={isLoading ? [] : data?.data} />
-//     </div>
-//   );
-// };
-
-// export default kategori;
-
-type ProductType = {
-  id: number;
-  name: string;
-};
-
-const TampilanProduk = ({ products = [] }: { products?: ProductType[] }) => {
   return (
     <div>
-      <h1 data-testid="title">Product Page</h1>
-
-      {products?.length > 0 ? (
-        products.map((product) => (
-          <p key={product.id}>{product.name}</p>
-        ))
-      ) : (
-        <p data-testid="empty">No Product</p>
-      )}
+      <TampilanProduk products={isLoading ? [] : data?.data} />
     </div>
   );
 };
 
-export default TampilanProduk;
+// export default kategori;
+
+// type ProductType = {
+//   id: number;
+//   name: string;
+// };
+
+// const TampilanProduk = ({ products = [] }: { products?: ProductType[] }) => {
+//   return (
+//     <div>
+//       <h1 data-testid="title">Product Page</h1>
+
+//       {products?.length > 0 ? (
+//         products.map((product) => (
+//           <p key={product.id}>{product.name}</p>
+//         ))
+//       ) : (
+//         <p data-testid="empty">No Product</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default TampilanProduk;
 
